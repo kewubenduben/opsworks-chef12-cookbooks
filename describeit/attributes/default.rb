3748b26cf66d3729e7450d2f7ac5ece4f63460b7
Ker Ruben Ramos <Ker Ruben Ramos>
2016-08-25 12:07:56 +0800

    template and defaults

--- describeit/attributes/default.rb
+++ describeit/attributes/default.rb
@@ -10,3 +10,10 @@ default['redis']['version'] = '3.2.3'
 default['ssl']['cert'] = 'snake-oil'
 
 default['app'] = 'describeit-sample-app'
+
+default['stage'] = 'development'
+
+default['db']['hostname'] = 'dbhost'
+default['db']['database'] = 'dbname'
+default['db']['user']['name'] = 'dbuser'
+default['db']['user']['password'] = 'dbpassword'
