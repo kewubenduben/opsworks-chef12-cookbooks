7a0ee1bb54e55ef7b16ad66fe2041e3f1020a983
Ker Ruben Ramos <Ker Ruben Ramos>
2016-08-26 22:19:55 +0800

    nginx now only listens to port 80

--- describeit/attributes/default.rb
+++ describeit/attributes/default.rb
@@ -7,8 +7,6 @@ default['nodejs']['version'] = '4.5.0'
 
 default['redis']['version'] = '3.2.3'
 
-default['ssl']['cert'] = 'snake-oil'
-
 default['app'] = 'describeit-sample-app'
 
 default['stage'] = 'development'
